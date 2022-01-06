e50e57d6179b138a2c1d44b85a01ea3492e8ff48
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-01-07 01:14:46 +0300

    inishal

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const headerBurgerMenu = document.querySelector('.menu-burger')
+const headerMenu = document.querySelector('.menu')
+
+
+headerBurgerMenu.addEventListener('click', () => {
+    headerBurgerMenu.classList.add('show')
+})
