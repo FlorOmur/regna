8309661b2f407cc96cb4c53585e10d8acf4a17b3
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-01-07 13:32:27 +0300

    hidden

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ const menuLinks = document.querySelectorAll('.menu-link')
 
 myBurger.addEventListener('click', () => {
     myMenu.classList.toggle('show')
+    document.body.classList.toggle('o-hidden')
 })
 
 menuLinks.forEach(oneLink => {
     oneLink.addEventListener('click', () => {
         myMenu.classList.remove('show')
-
+        document.body.classList.toggle('o-hidden')
     })
 })
